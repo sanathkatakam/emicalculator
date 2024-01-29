0ba805031545d4b6f7f4180ae6b024e160ca5358
Sanath Katakam <katakam.sanath@gmail.com>
2024-01-29 10:53:41 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -5,29 +5,42 @@ var output1 = document.getElementById("demo1");
 var output2 = document.getElementById("demo2");
 var output3 = document.getElementById("demo3");
 
-output1.innerHTML = slider1.value;
-output2.innerHTML = slider2.value;
-output3.innerHTML = slider3.value;
+output1.value = slider1.value;
+output2.value = slider2.value;
+output3.value = slider3.value;
 
 slider1.oninput = function() {
-  output1.innerHTML = this.value;
+  output1.value = this.value;
   var out1=output1;
 }
 slider2.oninput = function() {
-  output2.innerHTML = this.value;
+  output2.value = this.value;
   var out2=output2;
 }
 slider3.oninput = function() {
-  output3.innerHTML = this.value;
+  output3.value = this.value;
   var out3=output3;
 }
+
+output1.oninput = function() {
+  slider1.value = this.value;
+}
+
+output2.oninput = function() {
+  slider2.value = this.value;
+}
+
+output3.oninput = function() {
+  slider3.value = this.value;
+}
+
 document.getElementById("loan-form").addEventListener("submit", computeResults);
 
 function computeResults(e) {  
 
-  const UIamount = slider1.value;
-  const UIinterest = slider2.value;
-  const UIyears = slider3.value;
+  const UIamount = output1.value;
+  const UIinterest = output2.value;
+  const UIyears = output3.value;
 
   const principal = parseFloat(UIamount);
   const CalculateInterest = parseFloat(UIinterest) / 100 / 12;
@@ -49,10 +62,10 @@ function computeResults(e) {
 
   //Show results
 
-  document.getElementById("monthlyPayment").innerHTML = "₹ " + monthlyPayment;
+  document.getElementById("monthlyPayment").innerHTML = "&#8377;" + Number(monthlyPayment).toLocaleString();
 
-  document.getElementById("totalInterest").innerHTML = "₹ " + totalInterest;
+  document.getElementById("totalInterest").innerHTML = "&#8377;" + Number(totalInterest).toLocaleString();
 
-  document.getElementById("totalPayment").innerHTML = "₹ " + totalPayment;
+  document.getElementById("totalPayment").innerHTML = "&#8377;" + Number(totalPayment).toLocaleString();
     e.preventDefault();
 }
